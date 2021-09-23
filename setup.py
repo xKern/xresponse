2ad1e6d86bc59514e522f588caeddc002ddb25b3
Haider <me@haiderali.dev>
2021-09-23 21:45:35 +0530

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 DESCRIPTION = 'Reusable Responses and Error responses for xKern APIs'
 LONG_DESCRIPTION = Path('README.md').read_text()
 
